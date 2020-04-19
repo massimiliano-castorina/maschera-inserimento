ef6bc48f749a8382f226eefdcce99d39dc5272b6
soffino <coach33_44@msn.com>
2020-04-19 11:03:33 +0200

    Initial commit

---
+++ mask/src/test/java/com/insertmask/initial/MaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insertmask.initial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
